f5cacc3c74a5e4ddd046f76ae69311f3bc8da663
Sean Huang <hyprul@ucla.edu>
2018-07-29 00:40:19 -0500

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -2,9 +2,18 @@ require "bundler/setup"
 
 require "sinatra/activerecord"
 
+
+
 Bundler.require
 
-Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
+# Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
+#Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
+
+require_relative "../app/models/actor.rb"
+ require_relative "../app/models/character.rb"
+ require_relative "../app/models/network.rb"
+ require_relative "../app/models/show.rb"
 
 connection_details = YAML::load(File.open('config/database.yml'))
 ActiveRecord::Base.establish_connection(connection_details)
+
